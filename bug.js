6ca00846b87375ba921b75ba24fcecb1b9435ab1
Vedant Sabde <vedant.sabde20@vit.edu>
2023-12-15 23:24:17 +0530

    Made the changes in index.html file

---
+++ bug.js
@@ -0,0 +1,14 @@
+// Make sure you merge this file with master branch..
+
+async function doSomething(){
+    setTimeout(()=>{
+        console.log('Hey log something after 1 sec');
+    }, 1000);
+}
+
+let d = Date.now()
+console.log(d);
+
+
+
+
