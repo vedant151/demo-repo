d4bf21ef8cf79ede805aa29d68c0c13a9a4d0937
Vedant Sabde <vedant.sabde20@vit.edu>
2023-12-15 20:58:32 +0530

    async logic has been added

--- bug.js
+++ bug.js
@@ -1,1 +1,8 @@
-console.log('This is to fix the bug');
+async function doSomething(){
+    setTimeout(()=>{
+        console.log('Hey log something after 1 sec');
+    }, 1000);
+}
+
+let d = Date.now()
+console.log(d);
